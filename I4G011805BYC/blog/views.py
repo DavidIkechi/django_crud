362d6459d77bcecd79b135b2b5dd059590b81ac1
David Akwuru <daakwuru@gmail.com>
2022-06-23 12:55:35 +0100

    Created a project with my zuri id, add an app called blog, and created a generic view to manipulate the blog app

---
+++ I4G011805BYC/blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView, DetailView
+from .models import *
+
+# Create your views here.
+# creating the class based views
+# creating the class based view PostListView to inherit the ListView Generic class behavior
+class PostListView(ListView):
+    model = Post
+    
+# creating the class based view PostCreateView to inherit the CreateView Generic class behavior
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+  
+# creating the class based view PostDetailView to inherit the DetailView Generic class behavior
+class PostDetailView(DetailView):
+    model = Post
+    
+# creating the class based view PostUpdateView to inherit the UpdateView Generic class behavior
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    
+# creating the class based view PostDeleteView to inherit the DeleteView Generic class behavior
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("blog:all")
